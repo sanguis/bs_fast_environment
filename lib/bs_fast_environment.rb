e8fea857740ce96bbfe83b6542ca6af214df8732
Josh Beauregard <josh.beauregard@knectar.com>
2014-10-09 22:59:28 -0400

    created bin file. split up old singe file in to class and procedures

--- lib/bs_fast_environment.rb
+++ lib/bs_fast_environment.rb
@@ -10,84 +10,6 @@
 class Bs_Fast_Envronment
   require 'rubygems'
   require 'securerandom'
-  require 'dotenv'
-  require 'fileutils'
-  require 'optparse'
-
-  #load options from local file
-  Dotenv.load
-
-  options = {
-    :client => nil,
-    :instance => ENV['instance'],
-    :files => ENV['files'],
-    :php_version => ENV['php_version'],
-    :php_user => ENV['php_user'],
-    :app_owner => ENV['app_owner'],
-    :mysql_user => ENV['mysql_user'],
-    :private_files => ENV["private_files"]
-  }
-
-  operations = {
-    :mk_db => TRUE,
-    :mk_file_system => TRUE,
-    :setup_bs => TRUE,
-    :mk_vhost => TRUE
-  }
-
-  parser = OptionParser.new do|opts|
-    opts.banner = "new_drupal_app.rb [options]. All settings can be preset in a file called settings.rb"
-    opts.on('-c' ,'--client client', "Client value should match beanstalk project name") do |client|
-      options[:client] = client
-    end
-    opts.on('-i', '--instance instance', "The applications instance name. Defaults to 'dev'. If it is other than stage or dev it the URL will be INSTANCE-CLIENT.dev.knectar.com") do |instance|
-      options[:instance] = instance
-    end
-    opts.on('-f', '--files path', 'Path to Drupal files directory') do |files|
-      options[:files] = files;
-    end 
-    opts.on( "-r", "--private_files private_files", "The Drupal private files directory. Defaults to /sites/default/files/_private") do |opt|
-      options[:private_files]  = opt
-    end
-    opts.on('-p', '--php_version php_version', 'can be "5.3", "5.4", "5.5". Defaults to 5.4') do |ver|
-      options[:php_version] = ver;
-    end
-    opts.on( "-u", "--php_user",
-            "the user that php runs as") do |opt|
-      options[:php_user] = opt
-    end
-    opts.on( "-o", "--owner app owner", "The UNIX level owner that the application will live under, can be hard set in the settings") do |opt| 
-      options[:app_owner] = opt;
-    end
-    opts.on( "-M", "--no-db no-db", "Do not create database") do
-      operations[:mk_db] = FALSE
-    end
-    opts.on( "-F", "--no-directory no-directory", "Do not create directory structure") do
-      operations[:mk_file_system] = FALSE
-    end
-    opts.on( "-B", "--no-beanstalk", "Do not create beanstalk environment.") do 
-      operations[:setup_bs] = FALSE
-    end
-    opts.on( "-V", "--no-vhost no-vhost", "Do not create an Nginx virtual host for this application instance.") do
-      operations[:create_vhost] = FALSE
-    end
-    opts.on( "-O", "--options",
-            "Output all settings") do
-      require 'yaml'
-      puts YAML::dump(options)
-    end
-    opts.on('-h', '--help', 'Displays help') do
-      puts opts
-      exit
-    end
-  end
-
-  parser.parse!
-  if options[:client] == nil
-    print 'you need to at least enter a client name: '
-    options[:client] = gets.chomp
-  end
-
 
   def mk_file_system(options, app_template ='drupal')
     FileUtils.mkdir_p options[:client] + '/' + options[:instance] +'/' + options[:files]
@@ -175,15 +97,4 @@ class Bs_Fast_Envronment
     end
   end
 
-  if operations[:mk_file_system] == true
-    mk_file_system(options)
-  end  
-
-  if operations[:mk_db]
-    mk_db(options)
-  end
-
-  if operations[:mk_vhost]
-    mk_vhost(options)
-  end
 end
