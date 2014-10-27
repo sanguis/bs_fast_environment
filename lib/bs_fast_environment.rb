e3fdba59a3514692d6079886ec5483e5302d8f75
Josh Beauregard <josh.beauregard@knectar.com>
2014-10-27 02:14:30 -0400

    created overrideable yaml settings

--- lib/bs_fast_environment.rb
+++ lib/bs_fast_environment.rb
@@ -8,14 +8,14 @@ class Bs_Fast_Envronment
   end
 
   def self.mk_file_system(options, app_template ='drupal')
-    FileUtils.mkdir_p "#{options[:client]}/#{options[:instance]}/#{options[:files]}"
-    FileUtils.chown options[:app_owner], options[:app_owner], options[:client]
-    FileUtils.chown_R options[:app_owner], options[:app_owner], "#{options[:client]}/#{options[:instance]}"
+    FileUtils.mkdir_p "#{options[client]}/#{options[instance]}/#{options[files]}"
+    FileUtils.chown options[app_owner], options[app_owner], options[client]
+    FileUtils.chown_R options[app_owner], options[app_owner], "#{options[client]}/#{options[instance]}"
     begin
-      FileUtils.chown_R options[:php_user], options[:app_owner], options[:client] + '/' + options[:instance] +'/' + options[:files] 
+      FileUtils.chown_R options[php_user], options[app_owner], options[client] + '/' + options[instance] +'/' + options[files] 
     rescue
-      puts "Can not change 'files' directory owner to #{options[:php_user]}.\n Please enter sudoers password:"
-      system("sudo chown -R #{options[:php_user]}:#{options[:app_owner]} #{options[:client]}/#{options[:instance]}/#{options[:files]}")
+      puts "Can not change 'files' directory owner to #{options[php_user]}.\n Please enter sudoers password:"
+      system("sudo chown -R #{options[php_user]}:#{options[app_owner]} #{options[client]}/#{options[instance]}/#{options[files]}")
     end
     puts "File system prepared"
   end
@@ -24,12 +24,12 @@ class Bs_Fast_Envronment
     # making MySQL info
     require "sequel"
     random_password = SecureRandom.hex(20)
-    new_db = "#{options[:client]}_#{options[:instance]}"
-    print "What is the mysql #{options[:mysql_user]}'s password? "
+    new_db = "#{options[client]}_#{options[instance]}"
+    print "What is the mysql #{options[mysql_user]}'s password? "
     sql_password = gets.chomp
 
     puts "you entered : #{sql_password}"
-    root_connect = Sequel.connect("mysql://#{options[:mysql_user]}:#{sql_password}@localhost")
+    root_connect = Sequel.connect("mysql://#{options[mysql_user]}:#{sql_password}@localhost")
     begin
       root_connect.use(new_db)
     rescue 
@@ -49,25 +49,25 @@ class Bs_Fast_Envronment
 
   def self.mk_vhost(options)
 
-    File.open("/etc/nginx/sites-enabled/#{options[:cleint]}_#{options[:instance]}", 'w') do |f|
+    File.open("/etc/nginx/sites-enabled/#{options[cleint]}_#{options[instance]}", 'w') do |f|
       f.puts vhost_drupal(options)
     end
     system "sudo service nginx reload"
   end
 
   def self.vhost_drupal(options)
-    case options[:instance]
+    case options[instance]
     when "dev"
-      subdomain = "#{options[:client]}.#{options[:instance]}"
+      subdomain = "#{options[client]}.#{options[instance]}"
     when "stage"
-      subdomain = "#{options[:client]}.#{options[:instance]}"
+      subdomain = "#{options[client]}.#{options[instance]}"
     else
-      subdomain = "#{options[:instance]}-#{options[:client]}.dev"
+      subdomain = "#{options[instance]}-#{options[client]}.dev"
     end
     full_domain = "#{subdomain}.knectar.com"
 
     # setting the php version;
-    case options[:php_version]
+    case options[php_version]
     when "5.3"
       php_socket = "php_fpm"
     when "5.4"
@@ -75,7 +75,7 @@ class Bs_Fast_Envronment
     when "5.5"
       php_socket = "php55_fpm"
     else 
-      puts "The value #{options[:php_version]} is invalid. Please enter 5.3, 5.4 or 5.5 other options will fail."
+      puts "The value #{options[php_version]} is invalid. Please enter 5.3, 5.4 or 5.5 other options will fail."
       exit
     end
     puts "Creating the vhost file for http://#{full_domain}. It will run on the php socket #{php_socket}."
@@ -83,9 +83,9 @@ class Bs_Fast_Envronment
 #the URL
   server_name #{subdomain}.knectar.com;
 #path to the local host
-  root /home/sites/#{options[:client]}/#{options[:instance]};
+  root /home/sites/#{options[client]}/#{options[instance]};
 #include the app template
-  set $private_dir #{options[:private_files]};
+  set $private_dir #{options[private_files]};
   set $php_socket #{php_socket};
   include /etc/nginx/apps/drupal;
 }"
